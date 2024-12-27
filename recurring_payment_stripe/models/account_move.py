ea452673b2f97e56bec3b15e04894485a7e5c2a9
Manuel Vinent Guilarte <53498470+mjavint@users.noreply.github.com>
2024-12-27 07:56:26 -0500

    [FIX] Fixed payment transaction and design modular (#6)

--- recurring_payment_stripe/models/account_move.py
+++ recurring_payment_stripe/models/account_move.py
@@ -17,6 +17,7 @@ class AccountMove(models.Model):
         payment on subscriptions.
         """
         for invoice in self:
+            res = super(AccountMove, self).action_register_payment()
             # Find the subscription associated with the invoice, if it exists
             subscription = invoice.subscription_id
 
@@ -24,20 +25,19 @@ class AccountMove(models.Model):
             if subscription and subscription.charge_automatically:
                 provider = subscription.provider_id
                 stripe.api_key = provider.stripe_secret_key
-                token = self.env["payment.token"].search(
-                    [("provider_id", "=", provider.id)]
-                )
+                token = self._create_token(subscription)
                 try:
                     # Create the PaymentIntent and confirm it immediately
                     payment_intent = stripe.PaymentIntent.create(
-                        # Stripe usa centavos
+                        # Stripe uses cents
                         amount=int(invoice.amount_total * 100),
                         currency=invoice.currency_id.name.lower(),
                         customer=token.provider_ref,
                         payment_method=token.stripe_payment_method,
-                        # Para pagos automáticos sin intervención del usuario
+                        automatic_payment_methods={"enabled": True},
+                        # For automatic payments without user intervention
                         off_session=True,
-                        # Confirmar el PaymentIntent inmediatamente
+                        # Confirm the PaymentIntent immediately
                         confirm=True,
                         metadata={"odoo_invoice_id": str(invoice.id)},
                     )
@@ -57,7 +57,7 @@ class AccountMove(models.Model):
                             "payment_method_id": self.env.ref(
                                 "account.account_payment_method_manual_in"
                             ).id,
-                            "ref": f"Stripe PaymentIntent {payment_intent['id']}",
+                            "ref": f"Stripe - {payment_intent['id']}",
                         }
                         payment = Payment.create(payment_vals)
                         payment.action_post()
@@ -75,7 +75,56 @@ class AccountMove(models.Model):
                     raise UserError(f"Stripe error: {e}") from e
 
             else:
-                return super(AccountMove, self).action_register_payment()
+                return res
+
+    def _create_token(self, subscription):
+        provider = subscription.provider_id
+        # Search for an existing payment token for the given provider and partner
+        token = self.env["payment.token"].search(
+            [
+                ("provider_id", "=", provider.id),
+                ("partner_id", "=", subscription.partner_id.id),
+            ],
+            limit=1,
+        )
+
+        # If no token exists, create a new one
+        if not token:
+            stripe.api_key = provider.stripe_secret_key
+
+            # Create a new Stripe customer
+            customer = stripe.Customer.create(
+                email=subscription.partner_id.email,
+                name=subscription.partner_id.name,
+                metadata={"odoo_subscription": str(subscription.name)},
+            )
+
+            # Create a new payment token in Odoo
+            new_token = self.env["payment.token"].create(
+                {
+                    "provider_id": provider.id,
+                    "partner_id": subscription.partner_id.id,
+                    "provider_ref": customer.id,
+                    "verified": True,
+                }
+            )
+
+            # Retrieve the default payment method for the customer,
+            # or create one if it doesn't exist
+            new_token.stripe_payment_method = (
+                stripe.PaymentMethod.list(customer=customer.id, type="card", limit=1)
+                .data[0]
+                .id
+                if stripe.PaymentMethod.list(
+                    customer=customer.id, type="card", limit=1
+                ).data
+                else stripe.Customer.create_source(customer.id, source="tok_visa").id
+            )
+
+            # Assign the new token to the variable
+            token = new_token
+
+        return token
 
     @api.model
     def cron_process_due_invoices(self):
