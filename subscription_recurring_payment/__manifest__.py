75ac6af14d28e48721132a742e75f8476c549550
mjavint <mjavint@gmail.com>
2024-12-27 07:53:12 -0500

    [FIX] Fixed payment transaction and design modular

--- subscription_recurring_payment/__manifest__.py
+++ subscription_recurring_payment/__manifest__.py
@@ -1,17 +1,14 @@
 {
-    "name": "Recurring Payment with Stripe",
+    "name": "Subscription Recurring Payment",
     "version": "16.0.1.0.0",
-    "summary": """Recurring Payment with Stripe""",
+    "summary": """ Subscription Recurring Payment """,
     "author": "Binhex, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "license": "AGPL-3",
     "category": "Subscription Management",
-    "depends": ["subscription_recurring_payment", "payment_stripe"],
-    "data": [
-        "data/ir_cron.xml",
-    ],
+    "depends": ["subscription_oca", "payment"],
+    "data": ["views/sale_subscription_views.xml"],
     "installable": True,
     "auto_install": False,
-    "external_dependencies": {"python": ["stripe"]},
     "maintainers": ["mjavint"],
 }
--- subscription_recurring_payment/models/sale_subscription.py
+++ subscription_recurring_payment/models/sale_subscription.py
@@ -7,6 +7,5 @@ class SaleSubscription(models.Model):
     charge_automatically = fields.Boolean(default=True)
     provider_id = fields.Many2one(
         string="Provider",
-        domain=[("code", "=", "stripe")],
         comodel_name="payment.provider",
     )
